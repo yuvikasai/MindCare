9802c2023354a3ae1e23ee67203843e4bb585ac8
Yuvika <154349102+yuvikasai@users.noreply.github.com>
2025-04-01 16:46:52 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Smooth scroll for buttons
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  });
+  
